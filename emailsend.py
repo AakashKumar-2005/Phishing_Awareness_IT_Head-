3fb76d0224c381885fcee6294e3b2225941a8eb4
Yamini00006 <146608879+Yamini00006@users.noreply.github.com>
2024-12-06 11:25:48 +0530

    cmtd

--- emailsend.py
+++ emailsend.py
@@ -10,7 +10,8 @@ SENDER_EMAIL = 'yamini582006@gmail.com'
 SENDER_PASSWORD = 'qemg fgtb lxzz ixbg'
 
 # Base URL for phishing links (replace with your ngrok public URL)
-TRACKING_URL = 'https://1d6f-2405-201-e006-1075-f99e-7f9d-ccc2-cb66.ngrok-free.app/track-click?email='
+TRACKING_URL = 'https://6330-2401-4900-2326-cb4b-f083-d151-a2e3-e814.ngrok-free.app/track-click?email='
+TRACKING_PIXEL_URL = 'https://6330-2401-4900-2326-cb4b-f083-d151-a2e3-e814.ngrok-free.app/track-view?email='
 
 # Read recipient list from CSV
 recipients = pd.read_csv('email_list.csv')
@@ -26,6 +27,8 @@ def send_emails():
         for _, row in recipients.iterrows():
             recipient_name = row['Name']  # Fetch name
             recipient_email = row['Email']
+            tracking_link = TRACKING_URL + recipient_email
+            tracking_pixel  = TRACKING_PIXEL_URL + recipient_email
             redirecting = '&redirect=https://aakashkumar-2005.github.io/Phishing_Awareness_IT_Head-/'
             tracking_link = TRACKING_URL + recipient_email + redirecting
 
@@ -43,6 +46,8 @@ def send_emails():
                 <p><a href="{tracking_link}" style="color: blue; text-decoration: underline;">Click Here to Update</a></p>
 
                 <p>Kindly complete this update as soon as possible, or your access to key systems may be temporarily restricted.</p>
+
+                <img src="{tracking_pixel}" width="1" height="1" alt="." style="display: none;" />
 
                 <p>Best regards,<br>
                 [IT Head Name]<br>
